b327a500179be4797983bd54d930adb7c1cf2078
FLORA JENNER <flora.jenner@gmail.com>
2023-06-04 14:42:36 +0000

    reference javascript to html elements

--- assets/script.js
+++ assets/script.js
@@ -25,3 +25,12 @@ const quizData = [
         answer: 0,
     },
 ];
+
+
+//getters
+const quizContainer = document.getElementById("quiz-container");
+const questionContainer = document.getElementById("question-container");
+const optionContainer = document.getElementById("option-container");
+const submitButton = document.getElementById("submit-btn");
+const resultContainer = document.querySelector("#result-container");
+const restart = document.getElementById("again");
