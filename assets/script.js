69050258d044a796e1a07138d182c489a8fd67ff
FLORA JENNER <flora.jenner@gmail.com>
2023-06-04 14:43:55 +0000

    add javascript variables

--- assets/script.js
+++ assets/script.js
@@ -34,3 +34,7 @@ const optionContainer = document.getElementById("option-container");
 const submitButton = document.getElementById("submit-btn");
 const resultContainer = document.querySelector("#result-container");
 const restart = document.getElementById("again");
+
+let currentQuestion = 0;
+let score = 0;
+restart.style.visibility = "hidden";
